6adb57607d5437b91c4fc348c5fc0b664046c709
Fatima367 <princess.fatima3275@gmail.com>
2024-11-04 17:16:11 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: false,
+  },
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
