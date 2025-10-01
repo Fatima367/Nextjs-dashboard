03c7eacb3a0f29d9c0b2456891cb486118fd3138
Fatima367 <170196704+Fatima367@users.noreply.github.com>
2025-10-01 12:05:37 +0500

    update

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -45,12 +45,15 @@ export default function Form({
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId &&
-              state.errors.customerId.map((error: string) => (
+            {state.errors?.customerId?.map(
+              (
+                error: string // FIX: Used optional chaining before .map
+              ) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
                 </p>
-              ))}
+              )
+            )}
           </div>
         </div>
 
@@ -72,12 +75,15 @@ export default function Form({
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
             <div id="amount-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.amount &&
-                state.errors.amount.map((error: string) => (
+              {state.errors?.amount?.map(
+                (
+                  error: string // FIX: Used optional chaining before .map
+                ) => (
                   <p className="mt-2 text-sm text-red-500" key={error}>
                     {error}
                   </p>
-                ))}
+                )
+              )}
             </div>
           </div>
         </div>
